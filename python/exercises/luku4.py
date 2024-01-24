ed66df81c021a0e803d6597795a47674996c06a2
Victor <victorgrinan@gmail.com>
2024-01-24 10:34:29 +0200

    4-2

--- python/exercises/luku4.py
+++ python/exercises/luku4.py
@@ -12,3 +12,44 @@ def ex4_1():
         
     for i in range(start, stop, step):
         print(f'{i} / {stop}')
+#ex.2
+def ex4_2():
+        askel = 2
+        for i in range(50,61):
+            print(f'{i}')
+        
+        for i in range(0,21):
+            if i % 3 == 0:
+                print(f'{i}')
+     
+        for i in range(1,100, askel):
+            if i % 3 == 0:
+                print(f'{i}')
+
+#ex.3
+def ex4_3():
+    pass
+#ex.4
+def ex4_4():
+    pass
+#ex.5
+def ex4_5():
+    pass
+#ex.6
+def ex4_6():
+    pass
+#ex.7
+def ex4_7():
+    pass
+#ex.8
+def ex4_8():
+    pass
+#ex.9 
+def ex4_9():
+    pass             
+#ex.10
+def ex4_10():
+    pass            
+
+
+ex4_3()
