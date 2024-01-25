352a5188b8d6b885a63fa5a971d1422e4af367f2
Victor <victorgrinan@gmail.com>
2024-01-25 10:01:10 +0200

    5-1 & 5-2

---
+++ python/exercises/luku5.py
@@ -0,0 +1,23 @@
+#ex.1
+rahamaara = 10.123123123
+def muotoile_euroiksi(amount):
+    euros = format(amount, '.2f')
+    euros += '€'
+    return euros
+
+# print(muotoile_euroiksi(rahamaara))
+
+
+#ex.2
+def summa(a, b, c, d, e):
+    return a + b + c + d + e
+
+def suurin(a, b, c):
+    suurin = a
+    if b > suurin:
+        suurin = b
+    if c > suurin:
+        suurin = c
+    return suurin
+
+#ex.3
