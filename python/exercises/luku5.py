a6b8c2c865916efa519d00ac6ab86c9e42f015ad
Victor <victorgrinan@gmail.com>
2024-01-25 10:02:13 +0200

    5-2

--- python/exercises/luku5.py
+++ python/exercises/luku5.py
@@ -11,7 +11,7 @@ def muotoile_euroiksi(amount):
 #ex.2
 def summa(a, b, c, d, e):
     return a + b + c + d + e
-
+    
 def suurin(a, b, c):
     suurin = a
     if b > suurin:
@@ -19,5 +19,13 @@ def suurin(a, b, c):
     if c > suurin:
         suurin = c
     return suurin
+
+def etaisyys(a, b):
+    alempi = a
+    suurin = b
+    if b < alempi:
+        alempi = b
+        suurin = a
+    return suurin - alempi
 
 #ex.3
