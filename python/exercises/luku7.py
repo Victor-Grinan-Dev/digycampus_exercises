5af16de1aa2fb00797e22bb26982fec3d6c0c93f
Victor <victorgrinan@gmail.com>
2024-01-29 10:45:32 +0200

    7-3, 7-4

--- python/exercises/luku7.py
+++ python/exercises/luku7.py
@@ -18,4 +18,35 @@ def ex2():
 
 #ex.3
 def ex3():
-    pass
+    for i in list_apinat:
+        print(i)
+        
+    for i in list_apinat:
+        if i == 'gorilla' or i == 'oranki':
+            print(i)
+
+#ex.4
+def ex4():  	
+    lista_a = ["omena", "banaani", "ananas"]
+    lista_b = ["apina", "gorilla", "simpanssi", "simpanssi"]
+    lista_c = ["koira", "kissa", "papukaija"]
+    lista_d = ["delfiini", "valas", "tonnikala"]
+
+    lista_a.extend(['Pekka', 'Haavisto'])
+    lista_b.remove('simpanssi')
+    lista_e = lista_b
+    lista_e.extend(lista_c)
+    lista_e.extend(lista_d)
+    lista_e.sort()
+
+    print(len(lista_a))
+    #Lista A pituus : 5↩
+    print(lista_b)
+    #Lista B : ['apina', 'gorilla', 'simpanssi']↩
+    print(lista_c)
+    #Lista C : ['koira', 'kissa', 'papukaija']↩
+    print(lista_d)
+    #Lista D : ['delfiini', 'valas', 'tonnikala']↩
+    print(lista_e)
+    #Lista E : ['apina', 'delfiini', 'gorilla', 'kissa', 'koira','papukaija', 'simpanssi', 'tonnikala', 'valas']
+ex4()
