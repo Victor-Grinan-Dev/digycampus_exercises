d14451e0975cc05818d113346485f26bacc17169
Victor <victorgrinan@gmail.com>
2024-01-29 11:29:29 +0200

    7-6, 7-7

--- python/exercises/luku7.py
+++ python/exercises/luku7.py
@@ -1,5 +1,8 @@
 #ex.1
 # Lst[ Initial : End : IndexJump ]
+import random
+
+
 def ex1():
     apinat =  ['apina', 'gorilla', 'simpanssi', 'ihminen', 'bonobo', 'oranki', 'hulokki', 'gibboni']
  
@@ -49,4 +52,50 @@ def ex4():
     #Lista D : ['delfiini', 'valas', 'tonnikala']↩
     print(lista_e)
     #Lista E : ['apina', 'delfiini', 'gorilla', 'kissa', 'koira','papukaija', 'simpanssi', 'tonnikala', 'valas']
-ex4()
+#ex.5
+def ex5():  	
+
+    random.seed(50)
+    arvosanat = list()
+    nimet = ['Anna', 'Antti', 'Cecilia', 'Emma', 'Eetu', 'Heikki', 'Janna']
+    for _ in range(15):
+        nimi = random.choice(nimet)
+        arvosana = random.randint(0,5)
+        arvosanat.append([nimi,arvosana])
+
+    vitosia = 0 
+    emmoja = 0
+
+    for record in arvosanat:
+        if record[0] == 'Emma':
+            emmoja += 1
+        if record[1] == 5:
+            vitosia += 1
+#ex.6
+def ex6():  	
+    tilaajat = ['Matti Mallikas', 'Simo Särmä', 'Tiina Terävä']
+    numerot = tuple(i for i in range(1,11))
+    mallinumero = tuple([4930])
+    tuple_tilaajat = tuple(tilaajat )
+    print(type(numerot))
+    print(type(mallinumero))
+    print(type(tuple_tilaajat))
+#ex.7
+def unique_count7(lista):  	
+    return len(dict.fromkeys(lista))
+
+#ex.8
+ainesto = ['mölyapina', 'kummituseläin', 'marakatti', 'bonobo','marakatti', 'mölyapina', 'oranki', 'kummituseläin', 'marakatti', 'apina','ihminen', 'kummituseläin', 'apina', 'bonobo', 'marakatti']
+def kaikki8(aineisto, haettava):
+    
+    result = []
+    for element in aineisto :
+        for item in haettava:
+            if element == item:
+                result.append(aineisto.index(element))
+    return result
+
+def ex8():  	
+    pass
+
+print(kaikki8(ainesto, ['marakatti', 'mölyapina',]))
