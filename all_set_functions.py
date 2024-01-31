af0bbcd2b45e0d900f5f4bdf75b8b0e4ccf5337f
Victor <victorgrinan@gmail.com>
2024-01-31 11:17:25 +0200

    9-1

---
+++ all_set_functions.py
@@ -0,0 +1,27 @@
+
+l1=[1,2,1,2,3]
+s1 = set(l1)
+
+#ADD & UPDATE
+s1.add(4)
+s2 = {2,3,4}
+s1.update(s2)
+# print(s1)
+
+## REMOVE & DISCARD
+s1.remove(4) # get error if value doesnt exist
+s1.discard(5)
+
+## INTERCECTION
+s3 = {4,5,6}
+s4 = {5,6,7}
+# print(s1.intersection(s3))
+# print(s1.intersection(s3, s4))
+
+## difference (value in that is _ not in _)
+print(s1.difference(s2))
+print(s2.difference(s1))
+print(s1.difference(s3,s4)) #find the value in s1 that is not in s3 or s4
+print(s4.difference(s3,s1))
+
+## symetric difference
--- python/exercises/luku9.py
+++ python/exercises/luku9.py
@@ -1,9 +1,34 @@
+import pickle
 myset = {1, 2, 3}
-mylist = [1, 2, 3]
-myset.add(4) 
+mylist = [2, 3]
+myset.add(3) 
 # print(myset)
 # print(len(myset))
-myset2 = set((1,2,3))
-print(myset2)
+myset2 = set(mylist)
+def pickle_sample():
+    print(myset)
+    'pickle.dump(), pickle.load()'
+    'numpy, pandas'
 
-pickle, numpy, pandas
+    # with open('test.pkl', 'wb') as file:
+    #     pickle.dump({'name':'victor', 'age':40}, file)
+
+    # with open('test.pkl', 'rb') as file:
+    #     d = pickle.load(file)
+    #     print(d)
+
+    print(myset2.issubset(myset))
+    print(myset.issuperset(myset2))
+
+#ex.1
+def ex_1():
+    lisattavat = ["toinen", "testikierros", "lisää sekalaisia juttuja", 2353, True, "moi"]
+    numerot = {i for i in range(1,11)}
+    numerot.remove(6)
+    numerot.update(lisattavat)
+    print(numerot)
+
+#ex.2
+def ex_2():
+    
+ex_2()
