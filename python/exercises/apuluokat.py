ce1c92189729a3d7d3f2ee0fde0fefe2d8cd4ef3
Victor <victorgrinan@gmail.com>
2024-02-03 12:31:59 +0200

    10-5

--- python/exercises/apuluokat.py
+++ python/exercises/apuluokat.py
@@ -1,4 +1,7 @@
-# kortin maa (esim. hertta)
+import random
+
+
+# maa
 class Suit:
     def __init__(self, name):
         self.__name = name
@@ -15,12 +18,15 @@ class PlayingCard:
 
         if 1 < value < 11:  # numerokortti
             self.__name = str(value)
-        elif 0 < value < 14:    # kuvakortti
+        elif 0 < value < 14:  # kuvakortti
             names = {1: "Ace", 11: "Jack", 12: "Queen", 13: "King"}
             self.__name = names[value]
 
     def get_value(self):
         return self.__value
+
+    def get_suit(self):
+        return self.__suit
 
     # tätä metodia kutsutaan, kun kutsutaan str(kortti), kortin ollessa PlayingCard-olio
     def __str__(self):
@@ -33,6 +39,52 @@ class PlayingCard:
         else:
             return False
 
-    # samoin kuin luokan hashaaminen (EI LIITY TEHTÄVÄÄN)
+    # luokan hashaaminen (EI LIITY TEHTÄVÄÄN)
     def __hash__(self):
         return hash(str(self))
+
+    # luokan vertaaminen itsensä kanssa (EI LIITY TEHTÄVÄÄN)
+    def __gt__(self, other):
+        return self.get_value() > other.get_value()
+
+
+# pakka
+class Deck:
+    def __init__(self, cards):
+        self.__cards = list(cards)
+        self.__dealt = set()
+
+    def create_52_deck(self):
+        # Maat
+        pata = Suit("Spade")
+        hertta = Suit("Heart")
+        risti = Suit("Club")
+        ruutu = Suit("Diamond")
+
+        maat = [pata, hertta, risti, ruutu]
+        pakka = list()
+
+        for m in maat:
+            for x in range(1, 14):
+                kortti = PlayingCard(m, x)
+                pakka.append(kortti)
+        self.__cards = pakka
+
+    def deal(self):
+        if len(self.__dealt) > 40:
+            return False
+
+        c = random.choice(self.__cards)
+        while c in self.__dealt:
+            c = random.choice(self.__cards)
+        self.__dealt.add(c)
+        return c
+
+    def shuffle(self):
+        self.__dealt = set()
+    
+    def __str__(self):
+        for card in self.__cards:
+            print(card)
+        phrase = f"{len(self.__cards)-len(self.__dealt)} cards left"
+        return phrase
--- python/exercises/luku10.py
+++ python/exercises/luku10.py
@@ -1,42 +1,10 @@
 import random
+
 from apuluokat import PlayingCard, Suit
+from apuluokat import Deck as t_deck
 
-class MyEmployee:
-   
-    days_since_raise = 0
-    __salary = 2000
-    __total_hours = 0
-
-    def __init__(self, name):
-        self.name = name
-
-    def complete_workday(self, hours):
-        self.__total_hours += hours
-        # self.__hours += hours 
-        # days = self.__hours // 6
-        # if self.__hours % 6 > 0:
-        #     self.__hours = self.__hours % 6
-        #     self.days_since_raise += days
-        if hours > 6:
-            self.days_since_raise += 1
-    
-    def eligible_for_raise(self):
-        return self.days_since_raise > 365 
-    
-    def give_raise(self, percentage_increase):
-        self.__salary += self.salary * (percentage_increase/100)
-        self.days_since_raise = 0
-        
-    @property
-    def salary(self):
-        return round(self.__salary, 1)
-    
-    @property
-    def hours(self):
-        return self.__total_hours
-
-class Employee:
-   
+#ex.1
+class Employee: 
     days_since_raise = 0
     salary = 2000
     hours = 0
@@ -55,6 +23,40 @@ class Employee:
     def give_raise(self, percentage_increase):
         self.salary += self.salary * (percentage_increase/100)
         self.days_since_raise = 0 
+
+class MyEmployee:
+    """This version makes more sense to me but the exercise ask different response"""
+    days_since_raise = 0
+    __salary = 2000
+    __total_hours = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def complete_workday(self, hours):
+        """Here is the difference with the answer above"""
+        self.__total_hours += hours
+        self.__hours += hours 
+        days = self.__hours // 6
+        if self.__hours % 6 > 0:
+            self.__hours = self.__hours % 6
+            self.days_since_raise += days
+    
+    def eligible_for_raise(self):
+        return self.days_since_raise > 365 
+    
+    def give_raise(self, percentage_increase):
+        self.__salary += self.salary * (percentage_increase/100)
+        self.days_since_raise = 0
+        
+    @property
+    def salary(self):
+        return round(self.__salary, 1)
+    
+    @property
+    def hours(self):
+        return self.__total_hours
+
 
 def test_ex_1():
     print("Testatan luokan koko toimintokirjoa!")
@@ -76,7 +78,6 @@ def test_ex_1():
                 print(employee.name, "is eligible for a raise, after having worked for", employee.hours, "hours!")
                 employee.give_raise(5)
                 print("His/Her salary is now : ", employee.salary, "(+{}%)".format(5))
-                
 
 def test_ex_1_once():
     Jani = Employee("Jani")
@@ -89,7 +90,6 @@ def luo_korttipakka(lista:bool=False):
     suits = ("Spade", "Heart", "Club", "Diamond")
     pack = set()
     
-    
     for suit_name in suits:
         suit = Suit(suit_name)
         for card_number in range(1,14):
@@ -98,11 +98,9 @@ def luo_korttipakka(lista:bool=False):
     if lista:
         list(pack)
     return pack
-            
+
+
 #ex.3
-# add
-# discard
-# intersection
 class Deck:
     dealt = set()
     def __init__(self, card_deck:list):
@@ -126,6 +124,17 @@ class Deck:
         self.dealt = ()
 
 #ex.4
+#The method to add to ex 4:
+def find_flush(self):
+    is_flush = set()
+    for card in self.__cards:
+        is_flush.add(card.get_suit())
+    if len(is_flush) == 1:
+        return True
+    return False
+
+
+#ex.5
 class Hand:
     def __init__(self):
         self.__cards = list()
@@ -134,26 +143,74 @@ class Hand:
     def add_card(self, card):
         self.__cards.append(card)
 
-    # tätä metodia kutsutaan, kun kirjoitetaan str(x), missä x on Hand-luokan esiintymä (instance)
+    def adjust_rank(self, new_rank):
+        if new_rank < self.__rank:
+            self.__rank = new_rank
+            return True
+        return False
+
+    def get_high_card(self):
+        high = -1
+        for c in self.__cards:
+            if c.get_value() > high:
+                high = c.get_value()
+            if c.get_value() == 1:
+                high = 14
+        return high
+
+    def find_pairings(self):
+        paria = 0
+        kolmosta = 0
+        nelosta = 0
+        cards_in_hand = []
+        
+        for card in self.__cards:
+            cards_in_hand.append(card.get_value())
+        uniques = list(set(cards_in_hand))
+
+        for card in uniques:
+            count = cards_in_hand.count(card)   
+            if int(count) == 2:
+                paria += 1
+            elif count == 3:
+                kolmosta += 1
+            elif count >= 4:
+                nelosta += 1
+        
+        return paria, kolmosta, nelosta
+       
     def __str__(self):
         sorted_cards = sorted(self.__cards)
         str_representation = ""
         for c in sorted_cards:
             str_representation += str(c) + ", "
-        return str_representation
+        return str_representation.rstrip(', ')
+    
+def test_5():
+    # cards = 
+    my_deck = t_deck(luo_korttipakka(True))
+    # print(my_deck)
 
-    # metodi, joka palauttaa True jos kaikki käden kortit ovat samaa maata, False muuten
-    def find_flush(self):
-        is_flush = set()
-        for card in self.__cards:
-            is_flush.add(card.get_suit())
-        if len(is_flush) == 1:
-            return True
-        return False
-
+    for testi in range(100):
+        my_deck.shuffle()
+        hand = Hand()
+        hand.add_card(my_deck.deal())
+        hand.add_card(my_deck.deal())
+        hand.add_card(my_deck.deal())
+        hand.add_card(my_deck.deal())
+        hand.add_card(my_deck.deal())
+        
+        parit, kolmoset, neloset = hand.find_pairings()
+        # print(hand.find_pairings())
+        if parit > 1 or kolmoset == 1 or neloset == 1:
+            print("Käsi nro", testi, ":", hand)
+            print("Pareja:", parit)
+            print("Kolmosia:", kolmoset)
+            print("Nelosia:", neloset)
 
 if __name__ == '__main__':
     # test_ex_1()
     # test_ex_1_once()
     # luo_korttipakka()
+    test_5()
     pass
